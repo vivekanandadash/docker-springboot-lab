a830e412ea8aef99d4fcbf71eda8281f180e494c
Vivekananda <vivekanandadash245@gmail.com>
2026-03-19 02:47:35 +0530

    add simple restAPI methodcall and add DOCKER File and configure it

---
+++ l1-beginner/project-1/simple-rest-api/src/main/java/com/vivek/simplerestapi/javaDockerApp/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.vivek.simplerestapi.javaDockerApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String hello(){
+     return "hello World from Docker ,we welcome you";
+    }
+}
