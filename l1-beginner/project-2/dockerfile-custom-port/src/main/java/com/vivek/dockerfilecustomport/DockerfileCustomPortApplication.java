c9d16290715bb1e5bfb1548bfc11e07d0d6c5691
Vivekananda <vivekanandadash245@gmail.com>
2026-03-18 19:16:00 +0530

    Added spring boot project:simple rest api

---
+++ l1-beginner/project-2/dockerfile-custom-port/src/main/java/com/vivek/dockerfilecustomport/DockerfileCustomPortApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.dockerfilecustomport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerfileCustomPortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerfileCustomPortApplication.class, args);
+	}
+
+}
---
+++ l1-beginner/project-2/dockerfile-custom-port/src/test/java/com/vivek/dockerfilecustomport/DockerfileCustomPortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.dockerfilecustomport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerfileCustomPortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
